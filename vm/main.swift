65ebdff04c69b8cd0335596054f8ec172003c2ee
jpaquim <joaopaquim@gmail.com>
2024-07-09 16:20:37 +0100

    Setup cwd and home shared directories

--- vm/main.swift
+++ vm/main.swift
@@ -53,6 +53,16 @@ configuration.storageDevices = [
 
 configuration.entropyDevices = [VZVirtioEntropyDeviceConfiguration()]
 
+let cwd = VZVirtioFileSystemDeviceConfiguration(tag: "cwd")
+cwd.share = VZSingleDirectoryShare(
+  directory: VZSharedDirectory(
+    url: URL(fileURLWithPath: FileManager.default.currentDirectoryPath), readOnly: false))
+let home = VZVirtioFileSystemDeviceConfiguration(tag: "home")
+home.share = VZSingleDirectoryShare(
+  directory: VZSharedDirectory(
+    url: FileManager.default.homeDirectoryForCurrentUser, readOnly: false))
+configuration.directorySharingDevices = [cwd, home]
+
 // creates a linux bootloader with the given kernel and initial ramdisk
 let bootLoader = VZLinuxBootLoader(kernelURL: kernelURL)
 bootLoader.initialRamdiskURL = initialRamdiskURL
