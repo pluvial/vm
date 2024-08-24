543b7c100b20a715d70a1ae4aa648bd48976c9db
jpaquim <joaopaquim@gmail.com>
2024-08-24 21:41:11 +0000

    Add script to build lua

---
+++ debian/build/lua.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -ex
+
+dir="lua-5.4.7"
+file="$dir.tar.gz"
+url="https://lua.org/ftp/$file"
+cwd=$(pwd)
+
+mkdir -p ~/.local/cache
+cd ~/.local/cache
+trap "cd $cwd" EXIT
+
+wget $url
+chmod +x $file
+
+mkdir -p ~/.local/src
+cd ~/.local/src
+tar xf ~/.local/cache/$file
+cd $dir
+
+# make
+sudo apt-get install libreadline-dev
+make linux-readline
+make test
+mkdir -p local
+make install INSTALL_TOP="$PWD/local"
