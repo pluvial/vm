846ba098520a534efb414e16a71651c9b542a882
jpaquim <joaopaquim@gmail.com>
2024-08-26 17:32:00 +0000

    Add script to setup caddy

---
+++ debian/setup/caddy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -ex
+
+# sudo apt-get install caddy
+
+cwd=$(pwd)
+
+go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+
+mkdir -p ~/.local/opt/caddy
+cd ~/.local/opt/caddy
+trap "cd $cwd" EXIT
+
+xcaddy build
+cd ~/.local/bin
+ln -s ~/.local/opt/caddy/caddy
