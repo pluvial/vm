ceed381ba8a1ad97817f6a6a9ff9e46e4c64318b
jpaquim <joaopaquim@gmail.com>
2024-08-30 12:21:24 +0100

    Use install script to setup rustup

--- debian/setup/rust.sh
+++ debian/setup/rust.sh
@@ -2,10 +2,23 @@
 
 set -ex
 
-sudo apt-get install rustup
-rustup default stable
-echo 'export PATH="$HOME/.cargo/bin:$PATH"' >>~/.profile
-echo 'fish_add_path -p $HOME/.cargo/bin' >>~/.config/fish/config.fish
+# sudo apt-get install rustup
+# rustup default stable
+# echo 'export PATH="$HOME/.cargo/bin:$PATH"' >>~/.profile
+# echo 'fish_add_path -p $HOME/.cargo/bin' >>~/.config/fish/config.fish
+
+file="install.sh"
+url="https://sh.rustup.rs"
+cwd=$(pwd)
+
+mkdir -p ~/.local/cache/rustup
+cd ~/.local/cache/rustup
+trap "cd $cwd" EXIT
+
+wget -O $file $url
+chmod +x $file
+
+./$file
 
 . ~/.profile
 
