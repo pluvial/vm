5b40196b5b2f985f37a94745929c1e42939a82d2
jpaquim <joaopaquim@gmail.com>
2024-09-02 23:26:32 +0000

    Build zig master

--- debian/build/zig.sh
+++ debian/build/zig.sh
@@ -7,6 +7,26 @@ cwd=$(pwd)
 cd ~/.local/src/zig-bootstrap
 trap "cd $cwd" EXIT
 
-# CMAKE_GENERATOR=Ninja ./build native-linux-gnu native
-# CMAKE_GENERATOR=Ninja ./build native-linux-musl native
-CMAKE_BUILD_PARALLEL_LEVEL=4 ./build native-linux-musl native
+arch=native
+os=linux
+# abi=gnu
+abi=musl
+triple=$arch-$os-$abi
+mcpu=native
+
+# ./build $triple $mcpu
+# CMAKE_BUILD_PARALLEL_LEVEL=2 ./build $triple $mcpu
+CMAKE_GENERATOR=Ninja ./build $triple $mcpu
+
+ZIG_PREFIX="$PWD/out/zig-$triple-$mcpu"
+# LLVM_PREFIX="$PWD/out/$triple-$mcpu"
+# LLVM_PREFIX="$PWD/out/build-llvm-$triple-$mcpu"
+LLVM_PREFIX="$PWD/out/host"
+
+cd ~/.local/src/zig
+
+"$ZIG_PREFIX/zig" build \
+  -p stage3 \
+  --search-prefix "$LLVM_PREFIX" \
+  --zig-lib-dir lib \
+  -Dstatic-llvm
