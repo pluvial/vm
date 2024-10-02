2f54d4568b37863c5410018191ee5cd904bac587
jpaquim <joaopaquim@gmail.com>
2024-10-02 10:52:08 +0000

    Do not use docker install script due to release name conflict

--- scripts/setup/docker.sh
+++ scripts/setup/docker.sh
@@ -4,35 +4,33 @@ set -ex
 
 # sudo apt-get -y install docker.io docker-cli docker-compose
 
-# sudo apt-get -y remove docker.io docker-doc docker-compose podman-docker containerd runc
-# sudo apt-get update
-# sudo apt-get -y install ca-certificates curl
-# sudo install -m 0755 -d /etc/apt/keyrings
-# sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-# sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo apt-get -y remove docker.io docker-doc docker-compose podman-docker containerd runc
+sudo apt-get update
+sudo apt-get -y install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# echo \
-#   "deb [arch=arm64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian bullseye stable" | \
-#   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-# cat <<EOF >/etc/apt/sources.list.d/docker.list
-# deb [arch=arm64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian bullseye stable
-# EOF
+echo \
+  "deb [arch=arm64 signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian bookworm stable" |
+  sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
-# sudo apt-get update
-# sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-file="install-docker.sh"
-url="https://get.docker.com"
-cwd=$(pwd)
+# file="install-docker.sh"
+# url="https://get.docker.com"
+# cwd=$(pwd)
+#
+# mkdir -p ~/.local/cache/docker
+# cd ~/.local/cache/docker
+# trap "cd $cwd" EXIT
+#
+# wget -O $file $url
+# chmod +x $file
+#
+# ./$file
 
-mkdir -p ~/.local/cache/docker
-cd ~/.local/cache/docker
-trap "cd $cwd" EXIT
-
-wget -O $file $url
-chmod +x $file
-
-./$file
-
-sudo apt-get -y install slirp4netns uidmap vpnkit
+sudo apt-get -y install slirp4netns uidmap
+# sudo apt-get -y install uidmap vpnkit
 dockerd-rootless-setuptool.sh install
