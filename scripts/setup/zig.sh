c464759317b493e3184289f07ffaaad647dd39b0
jpaquim <joaopaquim@gmail.com>
2024-10-02 12:29:04 +0000

    Read zig master version dynamically from index.json

--- scripts/setup/zig.sh
+++ scripts/setup/zig.sh
@@ -2,26 +2,29 @@
 
 set -ex
 
-version="0.13.0"
-# version="0.14.0-dev.658+4a77c7f25"
+cwd=$(pwd)
+
+mkdir -p ~/.local/cache/zig
+cd ~/.local/cache/zig
+trap "cd $cwd" EXIT
+
+wget https://ziglang.org/download/index.json
+
+# version="0.13.0"
+version=$(cat index.json | jq -r .master.version)
+# tarball=$(cat index.json | jq -r '.master.["aarch64-linux"].tarball')
 
 name="zig-linux-aarch64-$version"
 file="$name.tar.xz"
 
-url="https://ziglang.org/download/$version/$file"
-# url="https://ziglang.org/builds/$file"
-
-cwd=$(pwd)
-
-mkdir -p ~/.local/cache
-cd ~/.local/cache
-trap "cd $cwd" EXIT
+# url="https://ziglang.org/download/$version/$file"
+url="https://ziglang.org/builds/$file"
 
 wget $url
 
 mkdir -p ~/.local/opt
 cd ~/.local/opt
-tar xf ../cache/$file
+tar xf ../cache/zig/$file
 
 mkdir -p ~/.local/bin
 cd ~/.local/bin
