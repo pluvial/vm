c43e24bcc2e0d43186d4bde0749d851d9a60ca42
jpaquim <joaopaquim@gmail.com>
2024-10-15 19:47:36 +0000

    Add script to setup ruby using mise

---
+++ scripts/setup/ruby.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -ex
+
+sudo apt-get -y install libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
+
+mise use -g ruby@latest
