6dc3ce4f64e25f97d37eeab39370c8abf0880704
jpaquim <joaopaquim@gmail.com>
2024-10-18 15:55:30 +0000

    Install caddy from apt repo

--- scripts/setup/caddy.sh
+++ scripts/setup/caddy.sh
@@ -4,14 +4,21 @@ set -ex
 
 # sudo apt-get -y install caddy
 
-cwd=$(pwd)
+# sudo apt-get -y install debian-keyring debian-archive-keyring apt-transport-https curl
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt-get update
+sudo apt-get -y install caddy
 
-go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+# cwd=$(pwd)
 
-mkdir -p ~/.local/opt/caddy
-cd ~/.local/opt/caddy
-trap "cd $cwd" EXIT
-
-xcaddy build
-cd ~/.local/bin
-ln -s ~/.local/opt/caddy/caddy
+# go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+#
+# mkdir -p ~/.local/opt/caddy
+# cd ~/.local/opt/caddy
+# trap "cd $cwd" EXIT
+#
+# xcaddy build
+# xcaddy build --with github.com/caddy-dns/cloudflare
+# cd ~/.local/bin
+# ln -s ~/.local/opt/caddy/caddy
